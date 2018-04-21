5d5bed3b0cab3a047bb6257b78b2ff0258918a69
kiran reddy kancharla <kiran.kancharla92@gmail.com>
2018-04-21 17:52:15 -0400

    Update email_classification_dec_tree.py

--- email_classification_dec_tree.py
+++ email_classification_dec_tree.py
@@ -32,3 +32,18 @@ linreg.fit(X_train_dtm,y_train)
 y_pred = linreg.predict(X_test_dtm)
 print metrics.confusion_matrix(y_test,y_pred)
 print metrics.accuracy_score(y_test,y_pred)
+
+temp = []
+for i in range(len(df)):
+    value = list(df.loc[i,:])[0]
+    value = value.split(",")
+    print (len(value))
+    value[0] = value[0][1:]
+    value[-1] = value[0][:-1]
+    for j in range(len(value)):
+        value[j] = value[j][2:-1]
+        value[j] = value[j].split(":")
+        temp.append(value[j][0])
+dataframe = pd.DataFrame(temp)
+dataframe.columns = ['Name']
+print (len(dataframe))
